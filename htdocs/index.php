68f60d398ce0c5e2ca12b067579f435d2b087d60
JerlinJR <infodotjerlin@gmail.com>
2022-09-18 12:45:07 +0000

    Grunt Test

---
+++ htdocs/index.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/1.css">
+    <link rel="stylesheet" href="css/2.css">
+    <link rel="stylesheet" href="css/3.css">
+    <link rel="stylesheet" href="css/4.css">
+    <link rel="stylesheet" href="css/5.css">
+</head>
+<body>
+
+    <section id="screen1">
+
+    <p>Scroll down</p>
+
+    <nav>
+        <ul>
+    <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Team</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    </section>
+
+    <section id="screen2"></section>
+    <section id="screen3"></section>
+
+
+	<script src="/js/style.js"></script>
+
+</body>
+</html>
---
+++ project/grunt/Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt){
+
+    grunt.registerTask('helloworld',function(){
+        console.log("I am Running Grunt...");
+    });
+    grunt.registerTask('default',function(){
+        console.log("Default");
+    }); 
+
+
+};
+
